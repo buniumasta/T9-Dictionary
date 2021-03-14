b0dfab5c3f2c5b93916704c6b35257594357c0d7
Bartosz Ostrowski <buniumasta@gmail.com>
2021-03-14 22:10:36 +0100

    Doc: Readme edit.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # T9-Dictionary
 
-A long, long time ago, before the touchscreen keyboards of modern phones, primitive beings called humans typed words on their flip phones using numeric keypads. "How does this work?" you wonder. "Numbers aren't letters!" Indeed, they are not; however, each numeric key was marked with a collection of letters it could represent.
+***A long, long time ago, before the touchscreen keyboards of modern phones, primitive beings called humans typed words on their flip phones using numeric keypads. "How does this work?" you wonder. "Numbers aren't letters!" Indeed, they are not; however, each numeric key was marked with a collection of letters it could represent.***
+
+The Python program simulates T-9 dictionary behaviour on old mobiles.
+
+Recursive dictionary data structure is used for fast search
+Recursive search functions are implemented for data search
 
 # File Sources
-The data set originally comes from Google's [NGram](https://storage.googleapis.com/books/ngrams/books/datasetsv3.html) project, released under CC BY 3.0. Specifically, we're using the 1-grams of version 20090715.
+The data set originally comes from Google's [NGram](https://storage.googleapis.com/books/ngrams/books/datasetsv3.html) project, released under CC BY 3.0.
--- t9.py
+++ t9.py
@@ -1,5 +1,6 @@
 import helper
-#import gold
+
+#Parsing Data
 
 def parse_content(content):
     lines=content.splitlines()
@@ -10,6 +11,8 @@ def parse_content(content):
         my_lista=myline.split()
         dictionary[my_lista[0]]=int(my_lista[1])
     return dictionary
+
+#Building Search dictionary - recusrive method used
 
 def add_word(word,complete_word,value,tree):
     #print("word: {} co: {}".format(word,complete_word))
@@ -32,7 +35,7 @@ def make_tree(words):
 #    print(tree)
     return tree
 
-##############################
+# Prediction #####################################################################################################
 def search_leaves(tree,results):
     for key,branch in tree.items():
         #print("Key:{}, typ_branch:{} {}".format(key,type(branch),branch))
@@ -42,9 +45,9 @@ def search_leaves(tree,results):
 
         if type(branch) == type({}):
             search_leaves(branch,results)
-    #return results
 
-#returns second element of resutls -> used for sorting of list.
+
+#returns second element of resutls -> used for sorting result list.
 def take_second(elem):
     return elem[1]
 
@@ -53,55 +56,25 @@ def search_branch(word,tree,results):
     if len(word) > 0:
         if word[0] in tree.keys():
             search_branch(word[1:],tree[word[0]],results)
-            #if type(tree[word[0]]) == type(str()):
-            #    if tree[word[0]].startswith('$'):
-            #        list_result.append(tree[word[0]])
-            #else:
-            #    search_word(word[1:],tree,list_result)
         else:
             pass
             #print("Scenario1: KeyNotFoundinTree: {} Return Tree: {}".format(word[0],tree))
-            #search_leaves(tree,results)
     else:
-        #print("Scenario0: KeyFoundinTree: {} Return Tree: {}".format(word[0],tree[word[0]]))
-        #search_leaves(tree[word[0]],results)
         #print("Scenario0: KeyFoundinTree: {} Return Tree: {}".format(word,tree))
         search_leaves(tree,results)
 
 
-keymap = {
-    '2': 'abc',
-    '3': 'def',
-    '4': 'ghi',
-    '5': 'jkl',
-    '6': 'mno',
-    '7': 'pqrs',
-    '8': 'tuv',
-    '9': 'wxyz',
-}
-
-
 def search_by_number(numbers,search_word,tree,results):
     #print("numbers: {} search_word: {}".format(numbers,search_word))
-
     if len(numbers) > 0:
         my_digit=numbers[0]
 
-        for letter in keymap[my_digit]:
+        for letter in helper.keymap[my_digit]:
             search_by_number(numbers[1:],search_word+letter,tree,results)
             #print("Number: {} Letter: {}".format(my_digit,letter))
-        #if word[0] in tree:
-        #    add_word(word[1:],complete_word+word[0],value,tree[word[0]])
-        #else:
-        #    tree[word[0]]= {}
-        #    add_word(word[1:],complete_word+word[0],value,tree[word[0]])
     else:
         #print("I will be looking for: {}".format(search_word))
         search_branch(search_word,tree,results)
-
-        #tree[word[0]]={''.join('$'+complete_word+word[0]):value}
-        #print("Added: key:{} value:{}".format(word[0],tree[word[0]]))
-        #return tree
 
 
 def prediction(tree, numbers):
@@ -109,42 +82,37 @@ def prediction(tree, numbers):
     match_string=[]
 
     search_by_number(numbers,"",tree,results)
-
-    #search_string="b"
-    #print("Searching for: {}".format(search_string) )
-    #search_branch(search_string,tree,results)
     results.sort(key=take_second,reverse=True)
-    #print("Results: {}".format(results))
 
-    #print(results)
     return results
+
+# Main funciton #####################################################################################################
 
 if __name__ == '__main__':
     content = helper.read_content(filename='ngrams10k.txt')
 
-    # When you've finished implementing a part, remove the `gold.` prefix to check your own code.
-
     # PART 1: Parsing a string into a dictionary.
+    # Recursive structure & recurcive method used
     words = parse_content(content)
-    #print(words)
+
 
     # PART 2: Building a trie from a collection of words.
+    # Recursive structure & recurcive method used
     tree = make_tree(words)
 
     predictions = prediction(tree, "222")
     print(predictions)
-#    while True:
-#        # PART 3: Predict words that could follow
-#        numbers = helper.ask_for_numbers()
-#        predictions = predict(tree, numbers)
-#
-#        if not predictions:
-#            print('No words were found that match those numbers. :(')
-#        else:
-#            for prediction, frequency in predictions[:10]:
-#                print(prediction, frequency)
+    while True:
+        # PART 3: Predict words that could follow
+        numbers = helper.ask_for_numbers()
+        predictions = predict(tree, numbers)
 
-#        response = input('Want to go again? [y/N] ')
-#        again = response and response[0] in ('y', 'Y')
-#        if not again:
-#            break
+        if not predictions:
+            print('No words were found that match those numbers. :(')
+        else:
+            for prediction, frequency in predictions[:10]:
+                print(prediction, frequency)
+        response = input('Want to go again? [y/N] ')
+        again = response and response[0] in ('y', 'Y')
+        if not again:
+            break
