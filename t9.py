2e47ccb85bd36ac21af321f4df271dc81dbbe4ed
Bartosz Ostrowski <buniumasta@gmail.com>
2021-03-14 21:25:21 +0100

    Enhacement: Working!

--- t9.py
+++ t9.py
@@ -32,39 +32,92 @@ def make_tree(words):
 #    print(tree)
     return tree
 
-def search_leaf(tree,list_result):
-    if tree!={}:
-        for value in tree.values():
-            if type(value) == type(str()):
-                if value.startswith('$'):
-                    list_results.append(value)
-                #else:
-                #    search_leaf(value,)
-            else:
-                search_leaf(value,list_result)
+##############################
+def search_leaves(tree,results):
+    for key,branch in tree.items():
+        #print("Key:{}, typ_branch:{} {}".format(key,type(branch),branch))
+        if key[0]== '$':
+            #print("Appending to results: {} {}".format(key[1:],branch))
+            results.append([key[1:],branch])
 
+        if type(branch) == type({}):
+            search_leaves(branch,results)
+    #return results
 
-def search_word(word,tree,list_result):
+#returns second element of resutls -> used for sorting of list.
+def take_second(elem):
+    return elem[1]
+
+def search_branch(word,tree,results):
     #print("word: {} co: {}".format(word,complete_word))
-    if len(word) > 1:
-        if word[0] in tree:
-            if type(tree[word[0]]) == type(str()):
-                if tree[word[0]].startswith('$'):
-                    list_result.append(tree[word[0]])
-            else:
-                search_word(word[1:],tree,list_result)
+    if len(word) > 0:
+        if word[0] in tree.keys():
+            search_branch(word[1:],tree[word[0]],results)
+            #if type(tree[word[0]]) == type(str()):
+            #    if tree[word[0]].startswith('$'):
+            #        list_result.append(tree[word[0]])
+            #else:
+            #    search_word(word[1:],tree,list_result)
         else:
             pass
+            #print("Scenario1: KeyNotFoundinTree: {} Return Tree: {}".format(word[0],tree))
+            #search_leaves(tree,results)
     else:
-        search_leaf(tree,list_result)
+        #print("Scenario0: KeyFoundinTree: {} Return Tree: {}".format(word[0],tree[word[0]]))
+        #search_leaves(tree[word[0]],results)
+        #print("Scenario0: KeyFoundinTree: {} Return Tree: {}".format(word,tree))
+        search_leaves(tree,results)
 
 
-def predict(tree, numbers):
-    for number in numbers:
-        #helper.keymap[number]
-        list_results=search_word("band",tree,[])
-    return list_results
+keymap = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+}
 
+
+def search_by_number(numbers,search_word,tree,results):
+    #print("numbers: {} search_word: {}".format(numbers,search_word))
+
+    if len(numbers) > 0:
+        my_digit=numbers[0]
+
+        for letter in keymap[my_digit]:
+            search_by_number(numbers[1:],search_word+letter,tree,results)
+            #print("Number: {} Letter: {}".format(my_digit,letter))
+        #if word[0] in tree:
+        #    add_word(word[1:],complete_word+word[0],value,tree[word[0]])
+        #else:
+        #    tree[word[0]]= {}
+        #    add_word(word[1:],complete_word+word[0],value,tree[word[0]])
+    else:
+        #print("I will be looking for: {}".format(search_word))
+        search_branch(search_word,tree,results)
+
+        #tree[word[0]]={''.join('$'+complete_word+word[0]):value}
+        #print("Added: key:{} value:{}".format(word[0],tree[word[0]]))
+        #return tree
+
+
+def prediction(tree, numbers):
+    results=[]
+    match_string=[]
+
+    search_by_number(numbers,"",tree,results)
+
+    #search_string="b"
+    #print("Searching for: {}".format(search_string) )
+    #search_branch(search_string,tree,results)
+    results.sort(key=take_second,reverse=True)
+    #print("Results: {}".format(results))
+
+    #print(results)
+    return results
 
 if __name__ == '__main__':
     content = helper.read_content(filename='ngrams10k.txt')
@@ -78,8 +131,8 @@ if __name__ == '__main__':
     # PART 2: Building a trie from a collection of words.
     tree = make_tree(words)
 
-    predictions = predict(tree, "2234")
-    print(predicitons)
+    predictions = prediction(tree, "222")
+    print(predictions)
 #    while True:
 #        # PART 3: Predict words that could follow
 #        numbers = helper.ask_for_numbers()
--- tree.py
+++ tree.py
@@ -16,16 +16,7 @@
 #                    lookup[key] = set()
 #                lookup[key].add(word)
 
-keymap = {
-    '2': 'abc',
-    '3': 'def',
-    '4': 'ghi',
-    '5': 'jkl',
-    '6': 'mno',
-    '7': 'pqrs',
-    '8': 'tuv',
-    '9': 'wxyz',
-}
+
 
 def add_word(word,complete_word,value,tree):
     #print("word: {} co: {}".format(word,complete_word))
@@ -45,7 +36,7 @@ def build_tree(words):
     tree={}
     for word,value in words.items():
         my_tree=add_word(word,'',value,tree)
-    print(tree)
+    #print(tree)
     return tree
 
 def prediction(tree,numbers):
@@ -53,19 +44,89 @@ def prediction(tree,numbers):
 
 def search_leaves(tree,results):
     for key,branch in tree.items():
-        print("Key:{}, typ_branch:{} {}".format(key,type(branch),branch))
+        #print("Key:{}, typ_branch:{} {}".format(key,type(branch),branch))
         if key[0]== '$':
-            print("Appending to results: {} {}".format(key[1:],branch))
+            #print("Appending to results: {} {}".format(key[1:],branch))
             results.append([key[1:],branch])
 
         if type(branch) == type({}):
             search_leaves(branch,results)
-    return results
+    #return results
 
+#returns second element of resutls -> used for sorting of list.
 def take_second(elem):
     return elem[1]
 
+def search_branch(word,tree,results):
+    #print("word: {} co: {}".format(word,complete_word))
+    if len(word) > 0:
+        if word[0] in tree.keys():
+            search_branch(word[1:],tree[word[0]],results)
+            #if type(tree[word[0]]) == type(str()):
+            #    if tree[word[0]].startswith('$'):
+            #        list_result.append(tree[word[0]])
+            #else:
+            #    search_word(word[1:],tree,list_result)
+        else:
+            pass
+            #print("Scenario1: KeyNotFoundinTree: {} Return Tree: {}".format(word[0],tree))
+            #search_leaves(tree,results)
+    else:
+        #print("Scenario0: KeyFoundinTree: {} Return Tree: {}".format(word[0],tree[word[0]]))
+        #search_leaves(tree[word[0]],results)
+        #print("Scenario0: KeyFoundinTree: {} Return Tree: {}".format(word,tree))
+        search_leaves(tree,results)
 
+
+keymap = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+}
+
+
+def search_by_number(numbers,search_word,tree,results):
+    #print("numbers: {} search_word: {}".format(numbers,search_word))
+
+    if len(numbers) > 0:
+        my_digit=numbers[0]
+
+        for letter in keymap[my_digit]:
+            search_by_number(numbers[1:],search_word+letter,tree,results)
+            #print("Number: {} Letter: {}".format(my_digit,letter))
+        #if word[0] in tree:
+        #    add_word(word[1:],complete_word+word[0],value,tree[word[0]])
+        #else:
+        #    tree[word[0]]= {}
+        #    add_word(word[1:],complete_word+word[0],value,tree[word[0]])
+    else:
+        #print("I will be looking for: {}".format(search_word))
+        search_branch(search_word,tree,results)
+
+        #tree[word[0]]={''.join('$'+complete_word+word[0]):value}
+        #print("Added: key:{} value:{}".format(word[0],tree[word[0]]))
+        #return tree
+
+
+def prediction(tree, numbers):
+    results=[]
+    match_string=[]
+
+    search_by_number(numbers,"",tree,results)
+
+    #search_string="b"
+    #print("Searching for: {}".format(search_string) )
+    #search_branch(search_string,tree,results)
+    results.sort(key=take_second,reverse=True)
+    #print("Results: {}".format(results))
+
+    #print(results)
+    return results
 
 words={
   'ban': 10,
@@ -76,16 +137,22 @@ words={
   'dark' : 18,
   'dream': 7,
   'darker' : 15,
-  'dreams' : 178
+  'dreams' : 178,
+  'canada' : 35
 }
 
-print(words)
-print("_______________")
+#print(words)
+#print("_______________")
 tree=build_tree(words)
 
-print("Learning to climb trees.... ")
-results=search_leaves(tree,[])
-print(results)
-results.sort(key=take_second,reverse=True)
-print(results)
+results = prediction(tree, "2263")
+#print(predicitons)
+
+#print("Learning to climb trees.... ")
+#results=search_leaves(tree,[])
+#print(results)
+#sorting results to the highest likelyhood
+#results.sort(key=take_second,reverse=True)
+print("Final Results {}".format(results))
+#search_by_number
 #drzewo=myTree()
